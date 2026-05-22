c930e338f3f594c6a02aac4516735d2747412bc6
Phạm Tiến Hưng <hungpham772006@gmail.com>
2026-05-22 07:22:22 +0700

    commit code

--- src/main/java/com/re/session7/Session7Bai1Application.java
+++ src/main/java/com/re/session7/Session7Bai1Application.java
@@ -2,8 +2,10 @@ package com.re.session7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class Session7Bai1Application {
 
     public static void main(String[] args) {
